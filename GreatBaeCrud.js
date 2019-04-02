1379a7964f80ca013e9f29b7779bc149ae5f2e4d
Matthew Trabold <mct4240@gmail.com>
2019-04-02 13:58:37 -0500

    Updated

--- GreatBaeCrud.js
+++ GreatBaeCrud.js
@@ -4,6 +4,16 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "9908Rock!",
+  password: "",
   database: "greatBaeDB"
 });
+
+function connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  createProduct();
+});
+
+function readItems() {
+
+}
